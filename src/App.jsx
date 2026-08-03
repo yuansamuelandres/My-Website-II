3f4413d2217ad186bd5acf5787650902e7d80b27
Yuan <yuansamuelandres@gmail.com>
2026-08-04 00:20:38 +0300

    Added the route for the SH pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import {Routes, Route} from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import Tajweed from "./pages/HTML/Tajweed"
-import NavBar from "./components/NavBar"
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Tajweed from "./pages/HTML/Tajweed";
+import NavBar from "./components/NavBar";
+import SH from "./pages/HTML/SH/SH.jsx";
+import SH_1 from "./pages/HTML/SH/SH_1.jsx";
+import SH_2 from "./pages/HTML/SH/SH_2.jsx";
 
 function App() {
-
-
   return (
     <>
-    {/* <NavBar/> */}
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/html/tajweed" element={<Tajweed/>}/>
-    </Routes>
+      {/* <NavBar/> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/html/tajweed" element={<Tajweed />} />
+        <Route path="/html/SH" element={<SH />} />
+        <Route path="/html/SH/SH_1" element={<SH_1 />} />
+        <Route path="/html/SH/SH_2" element={<SH_2 />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
