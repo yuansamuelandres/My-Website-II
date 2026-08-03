724e66943e0cb7b56a67eed3d8ec3606125f4013
Yuan <yuansamuelandres@gmail.com>
2026-08-04 00:22:05 +0300

    Just organized the file with Prettier :)

--- src/components/LectureCard.jsx
+++ src/components/LectureCard.jsx
@@ -1,20 +1,31 @@
 import "../../CSS/lectures-global.css";
 import lectures from "../database/lectures.json";
 
-
-function LectureCard({source, color}) {
-  
+function LectureCard({ source, color }) {
   return (
     <div className="lecture-cards">
       {Object.entries(lectures[source]).map((key) => (
-        <a href={key[1].url} target="_blank" style={{ backgroundImage: `url(${key[1].img})`, boxShadow: `0 0 1rem 0 var(${color})` }}>
+        <a
+          href={key[1].url}
+          target="_blank"
+          style={{
+            backgroundImage: `url(${key[1].img})`,
+            boxShadow: `0 0 1rem 0 var(${color})`,
+          }}
+        >
           <div className={key[0]}>
-            <span style={{color: `var(${color})`}}>{key[0]}</span>
+            <span style={{ color: `var(${color})` }}>{key[0]}</span>
             {/* <a href={key[1].url} target="_blank"> */}
-              {/* <img src={key[1].img} alt="an image" /> */}
+            {/* <img src={key[1].img} alt="an image" /> */}
             {/* </a> */}
             <h3>
-              <a href={key[1].url} target="_blank" style={{color: `var(${color})`}}>{key[1].title}</a>
+              <a
+                href={key[1].url}
+                target="_blank"
+                style={{ color: `var(${color})` }}
+              >
+                {key[1].title}
+              </a>
             </h3>
           </div>
         </a>
