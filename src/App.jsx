22cbbf8bfde2dbed2b85006310dc38e7faab2fd0
Yuan <yuansamuelandres@gmail.com>
2026-08-06 12:12:20 +0300

    Revert "Added the route for the  pages"
    This reverts commit 16564647313053b33b518407c90452adf8b76169.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import NavBar from "./components/NavBar";
 import SH from "./pages/HTML/SH/SH.jsx";
 import SH_1 from "./pages/HTML/SH/SH_1.jsx";
 import SH_2 from "./pages/HTML/SH/SH_2.jsx";
-import D from "./pages/HTML/D/D.jsx";
-import D_1 from "./pages/HTML/D/D_1.jsx";
-import D_2 from "./pages/HTML/D/D_2.jsx";
 
 function App() {
   return (
@@ -19,9 +16,6 @@ function App() {
         <Route path="/html/SH" element={<SH />} />
         <Route path="/html/SH/SH_1" element={<SH_1 />} />
         <Route path="/html/SH/SH_2" element={<SH_2 />} />
-        <Route path="/html/D" element={<D />} />
-        <Route path="/html/D/D_1" element={<D_1 />} />
-        <Route path="/html/D/D_2" element={<D_2 />} />
       </Routes>
     </>
   );
