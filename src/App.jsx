f061b2f4440a6c6beb0985234cb3a439d449f72e
Yuan <yuansamuelandres@gmail.com>
2026-08-07 15:53:43 +0300

    Deleted the NavBar componenet for App.jsx for now, added the Lectures page, and activated the HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Tajweed from "./pages/HTML/Tajweed";
-import NavBar from "./components/NavBar";
 import SH from "./pages/HTML/SH/SH.jsx";
 import SH_1 from "./pages/HTML/SH/SH_1.jsx";
 import SH_2 from "./pages/HTML/SH/SH_2.jsx";
 import D from "./pages/HTML/D/D.jsx";
 import D_1 from "./pages/HTML/D/D_1.jsx";
 import D_2 from "./pages/HTML/D/D_2.jsx";
+import Lectures from "./pages/Lectures.jsx";
 
 function App() {
   return (
     <>
-      <NavBar />
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/lectures" element={<Lectures nav="lectures"/>} />
         <Route path="/html/tajweed" element={<Tajweed />} />
         <Route path="/html/SH" element={<SH />} />
         <Route path="/html/SH/SH_1" element={<SH_1 />} />
